1d6ccb30d06953342963a8f0179b714cb889eec4
Shayne Boyer <spboyer@live.com>
2018-05-03 15:11:22 -0400

    minor edits

--- src/namesweb/Models/Names.cs
+++ src/namesweb/Models/Names.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace namesweb.Models
 {
 public class Name
 {
-  public string ID { get; set; }
+  public Guid ID { get; set; }
   public string FirstName { get; set; }
   public string LastName { get; set; }
   public string FullName { get; set; }
